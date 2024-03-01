7772e57081e03e1a32c231342a8ba8c3068fa461
allan-nganga <allen.nganga7@gmail.com>
2024-03-01 05:05:33 +0300

    Initial commit. Defined multiple data types

---
+++ main.dart
@@ -0,0 +1,23 @@
+void main() {
+  //Declaring variables using different datatypes
+    int num1 = 100;//Integer datatype stores numeric values
+    print(num1);
+
+    //Double datatype stores decimal numbers
+    double num2 = 20.5;
+    print(num2);
+
+    //String datatype stores text data
+    String firstName = "Allan";
+    print(firstName);
+
+    //Lists stores multiple values without having to create multiple variables
+    List<String> names = ["John", "James", "Allan"];
+    print(names);
+
+    //Maps stores a set of values as key-value pairs
+    Map<String, int> ages = {'John': 27,
+    'Susan': 30,
+    'Doris': 22};
+    print(ages);
+}
