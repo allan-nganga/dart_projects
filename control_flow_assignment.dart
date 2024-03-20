bad039e2c1eba4335b9492f9529b530f22c90253
allan-nganga <allen.nganga7@gmail.com>
2024-03-20 15:08:05 +0300

    Dart control flow assignment

---
+++ control_flow_assignment.dart
@@ -0,0 +1,10 @@
+void main() {
+  int num1 = 10;
+  if (num1 > 10) {
+    print("Your number is greater than 10");
+  } else if (num1 < 10) {
+    print("Your number is less tha 10");
+  } else {
+    print("Your number is equal to 10");
+  }
+}
